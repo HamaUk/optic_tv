b81cb2982718074620c13759effefeb8869163f5
HamaUk <hama@optic.tv>
2026-04-14 00:56:35 +0100

    fix(android): inject fallback namespace into subprojects to resolve legacy plugin build errors

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -24,6 +24,15 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+
+    afterEvaluate {
+        if (plugins.hasPlugin("com.android.application") || plugins.hasPlugin("com.android.library")) {
+            val android = extensions.findByName("android") as? com.android.build.gradle.BaseExtension
+            if (android != null && android.namespace.isNullOrEmpty()) {
+                android.namespace = project.group.toString()
+            }
+        }
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
