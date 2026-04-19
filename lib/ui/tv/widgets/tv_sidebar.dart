e294e9963d8a1ad51513bc78d47c63d6139be477
HamaUk <hama@optic.tv>
2026-04-19 01:07:18 +0100

    Build Fix: Added missing GoogleFonts import in TV Sidebar

--- lib/ui/tv/widgets/tv_sidebar.dart
+++ lib/ui/tv/widgets/tv_sidebar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import '../../../../core/theme.dart';
 import '../../../../widgets/tv_fluid_focusable.dart';
 
